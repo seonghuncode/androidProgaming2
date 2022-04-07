9f411906e0027ba861b1db6743fcbbe5b2585d7c
seonghuncode <gnos456@naver.com>
2022-04-07 22:18:45 +0900

    xml파일과, mainActivity파일만 보이게 수정

--- Chapter5/app/src/main/java/com/cookandroid/chapter5/MainActivity.java
+++ Chapter5/app/src/main/java/com/cookandroid/chapter5/MainActivity.java
@@ -10,6 +10,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
+
 public class MainActivity extends AppCompatActivity {
 
     //전역 변수를 선언 해주는 작업
